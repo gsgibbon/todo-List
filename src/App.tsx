ea98c7f771e2355d52627df0f9d2c0ea379c0a0d
Guilherme Gibbon <gibbon309@hotmail.com>
2025-11-21 17:44:26 -0300

    chore: instalar vite-plugin-svgt e configurar tipagem com typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import Trash from "./assets/icons/trash.svg?react";
 import Text from "./components/text";
-
 
 export default function App() {
   return (
-    <div>
+    <div className="grid gap-3">
       <div className="flex flex-col gap-1">
         <Text variant="body-md-bold" className="text-green-base">
           Hello World
         </Text>
         <Text>Hello World</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Trash className="fill-green-base" />
+        <Trash className="fill-pink-dark" />
       </div>
     </div>
   )
