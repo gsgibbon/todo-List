aaed5981ff1742311650c3065b5e7dcc32f63d58
Guilherme Gibbon <gibbon309@hotmail.com>
2025-11-21 16:43:24 -0300

    feat: criar componente Text e instalar CVA

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Text from "./components/text";
 
 
 export default function App() {
-  return <h1 className="text-green-base">Hello World</h1>
+  return (
+    <div>
+      <div className="flex flex-col gap-1">
+        <Text variant="body-md-bold" className="text-green-base">
+          Hello World
+        </Text>
+        <Text>Hello World</Text>
+      </div>
+    </div>
+  )
 }
